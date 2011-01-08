1bd1fb53b35f52d7eef944d5b4b5105f32671fc1
SigmaVirus24 <graffatcolmingov@gmail.com>
2011-01-08 15:03:49 -0500

    Fixed bat_sig.py.

--- src/irc/bat_sig.py
+++ src/irc/bat_sig.py
@@ -5,4 +5,4 @@ for line in os.popen("ps fax | grep nabatsignal"):
 	if './nabatsignal' in line[-1:-15:-1][::-1].rstrip():
 		exit
 
-os.system('screen -d -m -S batsig ./nabatsignal')
+i = os.spawnlp(os.P_NOWAIT, '/usr/bin/screen', 'screen', '-d', '-m', '-S', 'batsig', './nabatsignal')
