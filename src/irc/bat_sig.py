57037f081f81a9a49455def116f766947374a363
SigmaVirus24 <graffatcolmingov@gmail.com>
2011-01-09 17:07:33 -0500

    Fixed python scripts.

--- src/irc/bat_sig.py
+++ src/irc/bat_sig.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env py
 import os
 
+running = False
 for line in os.popen("ps fax | grep nabatsignal"):
 	if './nabatsignal' in line[-1:-15:-1][::-1].rstrip():
-		exit
+		running = True
 
-i = os.spawnlp(os.P_NOWAIT, '/usr/bin/screen', 'screen', '-d', '-m', '-S', 'batsig', './nabatsignal')
+if not running:
+	i = os.spawnlp(os.P_NOWAIT, '/usr/bin/screen', 'screen', '-d', '-m', '-S', 'batsig', './nabatsignal')
--- src/irc/test.py
+++ src/irc/test.py
@@ -1,7 +1,9 @@
 import os
 
+running = False
 for line in os.popen("ps fax | grep test"):
 	if './test' in line[-1:-10:-1][::-1].rstrip():
-		exit
+		running = True
 
-i = os.spawnlp(os.P_NOWAIT, '/usr/bin/screen', 'screen', '-d', '-m', '-S', 'batsig', './test')
+if not running:
+	i = os.spawnlp(os.P_NOWAIT, '/usr/bin/screen', 'screen', '-d', '-m', '-S', 'batsig', './test')
